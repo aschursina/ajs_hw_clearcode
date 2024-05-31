95adec15d66274af26d1dffe9ee4c95cace427b7
Anastasia <n3636@mail.ru>
2024-05-31 21:23:11 +0300

    change test

--- src/tests/test.js
+++ src/tests/test.js
@@ -2,11 +2,11 @@ import HealthStatus from "../healthstatus";
 
 test.each([
 ['Mary', 80, 'healthy'],
-['Ivan', 35, 'bounded'],
+['Ivan', 35, 'wounded'],
 ['Lena', 5, 'critical'],
-])('test healthStatus with name %i and health %i',(name, health, expected) => {
+])('test healthStatus with name %s and health %i',(name, health, expected) => {
     const result = HealthStatus({name, health});
     
-    expected(result).toBe(expected);
-})
+    expect(result).toBe(expected);
+});
 
